4c41f25a9def1d7bcb89b363f791b56dc5b145eb
saif0110 <khanmohdtauheed4@gmail.com>
2024-12-05 16:42:20 +0530

    file changed

--- src/components/App.js
+++ src/components/App.js
@@ -21,11 +21,11 @@ export default App
 function List(){
   return (
     <div>
-      <h1>List Item</h1>
+      <h1>Item List</h1>
       <ul>
-        <li><Link to={"/item/1"}>item 1</Link></li>
-        <li><Link to={"/item/2"}>item 2</Link></li>
-        <li><Link to={"/item/3"}>item 3</Link></li>
+        <li><Link to={"/item/1"}>Item 1</Link></li>
+        <li><Link to={"/item/2"}>Item 2</Link></li>
+        <li><Link to={"/item/3"}>Item 3</Link></li>
       </ul>
     </div>
   )
